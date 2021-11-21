928581acf4580555bd7a1f2c1a882b1ecf6b8873
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2021-11-21 10:32:47 +0100

    Aktualizovat main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
-namerenaDoba = 0
-urcenaDoba = 0
-
-
-
 def on_button_pressed_a():
-    global urcenaDoba
-    basic.show_icon(IconNames.YES)
+    if(urcenaDoba == 0):
+            return
+    basic.show_icon(IconNames.TARGET)
     music.play_tone(Note.C, urcenaDoba)
-    basic.show_icon(IconNames.HEART)
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    """)
 
 def on_button_pressed_b():
     global urcenaDoba
-    urcenaDoba = randint(500, 5000)
+    urcenaDoba = randint(5, 50)*100
     on_button_pressed_a()
+
+def on_logo_touched():
+    if(urcenaDoba == 0):
+            return
+    global dobaStartu
+    dobaStartu = control.millis()
+input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
+
+def on_logo_released():
+    if(urcenaDoba == 0):
+        return
+    global namerenaDoba
+    namerenaDoba = control.millis() - dobaStartu
+    odchylka = urcenaDoba - namerenaDoba
+    basic.show_leds("""
+    #####
+    #####
+    #####
+    #####
+    #####
+    """)
+    for a in range((abs(odchylka//100))):
+        led.unplot(a//5, a-(a//5)*5)
+    if abs(odchylka/100) <= 1:
+        music.play_melody("C D E F A", 120)
+input.on_logo_event(TouchButtonEvent.RELEASED, on_logo_released)
+
+urcenaDoba = 0
+dobaStartu = 0
+namerenaDoba = 0
 
 def on_forever():
     if input.button_is_pressed(Button.A):
-        on_button_pressed_a
+        on_button_pressed_a()
     if input.button_is_pressed(Button.B):
-        on_button_pressed_b
+        on_button_pressed_b()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,19 +1,66 @@
-let namerenaDoba = 0
-let urcenaDoba = 0
 function on_button_pressed_a() {
+    if (urcenaDoba == 0) {
+        return
+    }
     
-    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Target)
     music.playTone(Note.C, urcenaDoba)
-    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 }
 
+function on_button_pressed_b() {
+    
+    urcenaDoba = randint(5, 50) * 100
+    on_button_pressed_a()
+}
+
+input.onLogoEvent(TouchButtonEvent.Touched, function on_logo_touched() {
+    if (urcenaDoba == 0) {
+        return
+    }
+    
+    
+    dobaStartu = control.millis()
+})
+input.onLogoEvent(TouchButtonEvent.Released, function on_logo_released() {
+    if (urcenaDoba == 0) {
+        return
+    }
+    
+    
+    namerenaDoba = control.millis() - dobaStartu
+    let odchylka = urcenaDoba - namerenaDoba
+    basic.showLeds(`
+    #####
+    #####
+    #####
+    #####
+    #####
+    `)
+    for (let a = 0; a < Math.abs(Math.idiv(odchylka, 100)); a++) {
+        led.unplot(Math.idiv(a, 5), a - Math.idiv(a, 5) * 5)
+    }
+    if (Math.abs(odchylka / 100) <= 1) {
+        music.playMelody("C D E F A", 120)
+    }
+    
+})
+let urcenaDoba = 0
+let dobaStartu = 0
+let namerenaDoba = 0
 basic.forever(function on_forever() {
     if (input.buttonIsPressed(Button.A)) {
-        on_button_pressed_a
+        on_button_pressed_a()
     }
     
     if (input.buttonIsPressed(Button.B)) {
-        on_button_pressed_b
+        on_button_pressed_b()
     }
     
 })
